fa20687b01b0f9cca3dffe05367459567e391615
PermaFreez <dunci063@gmail.com>
2022-04-03 21:22:18 +0200

    Implemented fixed timesteps, started work on target handling

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
-use bevy::prelude::*;
+use bevy::{
+    prelude::*,
+    core::FixedTimestep,
+};
+
+const TIME_STEP: f32 = 1.0 / 60.0;
 
 const BACKGROUND_COLOR: Color = Color::rgb(0.9, 0.7, 0.6);
 const PLAYER_SIZE: f32 = 47.0;
 const PLAYER_COLOR: Color = Color::rgb(0.0, 0.3, 0.4);
-const PLAYER_SPEED: f32 = 5.0;
+const PLAYER_SPEED: f32 = 300.0;
 const SCORE_FONT_SIZE: f32 = 60.0;
 const SCORE_FONT_COLOR: Color = Color::rgb(0.4, 0.5, 0.5);
+const MAX_TARGETS: u8 = 10;
 
 fn main() {
     App::new()
@@ -16,7 +22,12 @@ fn main() {
         .insert_resource(ClearColor(BACKGROUND_COLOR))
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup)
+        .add_system_set(
+            SystemSet::new()
+                .with_run_criteria(FixedTimestep::step(TIME_STEP as f64)),
+        )
         .add_system(move_player)
+        .add_system(handle_targets)
         .add_system(bevy::input::system::exit_on_esc_system)
         .run();
 }
@@ -26,6 +37,12 @@ struct ScoreBoard;
 
 #[derive(Component)]
 struct Player;
+
+#[derive(Component)]
+struct Target;
+
+#[derive(Component)]
+struct Collider;
 
 pub fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     // Spawning the camera
@@ -39,7 +56,7 @@ pub fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
         color: SCORE_FONT_COLOR,
     };
     let text_alignment = TextAlignment {
-        vertical: VerticalAlign::Center,
+        vertical: VerticalAlign::Top,
         horizontal: HorizontalAlign::Center,
     };
     commands
@@ -62,7 +79,8 @@ pub fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
             ..Default::default()
         },
         ..Default::default()
-    }).insert(Player);
+    }).insert(Player)
+    .insert(Collider);
 }
 
 fn move_player(keyboard_input: Res<Input<KeyCode>>, 
@@ -73,20 +91,34 @@ fn move_player(keyboard_input: Res<Input<KeyCode>>,
     let mut y_change: f32 = 0.0;
 
     if keyboard_input.pressed(KeyCode::Left) {
-        x_change -= PLAYER_SPEED;
+        x_change -= PLAYER_SPEED * TIME_STEP;
     }
     if keyboard_input.pressed(KeyCode::Right) {
-        x_change += PLAYER_SPEED;
+        x_change += PLAYER_SPEED * TIME_STEP;
     }
     if keyboard_input.pressed(KeyCode::Up) {
-        y_change += PLAYER_SPEED;
+        y_change += PLAYER_SPEED * TIME_STEP;
     }
     if keyboard_input.pressed(KeyCode::Down) {
-        y_change -= PLAYER_SPEED;
+        y_change -= PLAYER_SPEED * TIME_STEP;
     }
 
     player_transform.translation.x += x_change;
     player_transform.translation.y += y_change;
 }
     
+fn handle_targets(mut query: Query<&mut Transform, With<Target>>) {
+    let mut remaining_targets: u8 = 0;
 
+    for target in query.iter() {
+        remaining_targets += 1
+    }
+
+    if remaining_targets == 0 {
+        spawn_targets();
+    }
+}
+
+fn spawn_targets() {
+//    println!("Spawning new targets!");
+}
